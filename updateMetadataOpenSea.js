35c649f469a87950f0696eadc031efadc1cc9708
yourboyjus <100374383+yourboyjus@users.noreply.github.com>
2022-02-26 16:15:19 +0100

    Add files via upload

---
+++ updateMetadataOpenSea.js
@@ -0,0 +1,602 @@
+[
+    {
+        "background": "purple",
+        "hair": "blue",
+        "cover": "cool",
+        "tokenId": 0
+    },
+    {
+        "background": "blue",
+        "hair": "blue",
+        "cover": "cool",
+        "tokenId": 1
+    },
+    {
+        "background": "blue",
+        "hair": "orange",
+        "cover": "cool",
+        "tokenId": 2
+    },
+    {
+        "background": "orange",
+        "hair": "blue",
+        "cover": "fancy",
+        "tokenId": 3
+    },
+    {
+        "background": "purple",
+        "hair": "purple",
+        "cover": "cool",
+        "tokenId": 4
+    },
+    {
+        "background": "orange",
+        "hair": "blue",
+        "cover": "casual",
+        "tokenId": 5
+    },
+    {
+        "background": "orange",
+        "hair": "orange",
+        "cover": "casual",
+        "tokenId": 6
+    },
+    {
+        "background": "purple",
+        "hair": "yellow",
+        "cover": "cool",
+        "tokenId": 7
+    },
+    {
+        "background": "blue",
+        "hair": "orange",
+        "cover": "rock",
+        "tokenId": 8
+    },
+    {
+        "background": "blue",
+        "hair": "yellow",
+        "cover": "cool",
+        "tokenId": 9
+    },
+    {
+        "background": "blue",
+        "hair": "red",
+        "cover": "casual",
+        "tokenId": 10
+    },
+    {
+        "background": "purple",
+        "hair": "orange",
+        "cover": "cool",
+        "tokenId": 11
+    },
+    {
+        "background": "orange",
+        "hair": "orange",
+        "cover": "fancy",
+        "tokenId": 12
+    },
+    {
+        "background": "blue",
+        "hair": "orange",
+        "cover": "casual",
+        "tokenId": 13
+    },
+    {
+        "background": "blue",
+        "hair": "purple",
+        "cover": "cool",
+        "tokenId": 14
+    },
+    {
+        "background": "orange",
+        "hair": "blue",
+        "cover": "cool",
+        "tokenId": 15
+    },
+    {
+        "background": "purple",
+        "hair": "blue",
+        "cover": "fancy",
+        "tokenId": 16
+    },
+    {
+        "background": "orange",
+        "hair": "purple",
+        "cover": "cool",
+        "tokenId": 17
+    },
+    {
+        "background": "orange",
+        "hair": "yellow",
+        "cover": "fancy",
+        "tokenId": 18
+    },
+    {
+        "background": "blue",
+        "hair": "blue",
+        "cover": "love",
+        "tokenId": 19
+    },
+    {
+        "background": "red",
+        "hair": "red",
+        "cover": "rock",
+        "tokenId": 20
+    },
+    {
+        "background": "orange",
+        "hair": "yellow",
+        "cover": "rock",
+        "tokenId": 21
+    },
+    {
+        "background": "orange",
+        "hair": "orange",
+        "cover": "cool",
+        "tokenId": 22
+    },
+    {
+        "background": "yellow",
+        "hair": "purple",
+        "cover": "cool",
+        "tokenId": 23
+    },
+    {
+        "background": "blue",
+        "hair": "purple",
+        "cover": "fancy",
+        "tokenId": 24
+    },
+    {
+        "background": "blue",
+        "hair": "yellow",
+        "cover": "rock",
+        "tokenId": 25
+    },
+    {
+        "background": "purple",
+        "hair": "red",
+        "cover": "fancy",
+        "tokenId": 26
+    },
+    {
+        "background": "blue",
+        "hair": "yellow",
+        "cover": "fancy",
+        "tokenId": 27
+    },
+    {
+        "background": "orange",
+        "hair": "yellow",
+        "cover": "cool",
+        "tokenId": 28
+    },
+    {
+        "background": "orange",
+        "hair": "orange",
+        "cover": "rock",
+        "tokenId": 29
+    },
+    {
+        "background": "purple",
+        "hair": "purple",
+        "cover": "casual",
+        "tokenId": 30
+    },
+    {
+        "background": "orange",
+        "hair": "blue",
+        "cover": "rock",
+        "tokenId": 31
+    },
+    {
+        "background": "orange",
+        "hair": "orange",
+        "cover": "love",
+        "tokenId": 32
+    },
+    {
+        "background": "blue",
+        "hair": "red",
+        "cover": "cool",
+        "tokenId": 33
+    },
+    {
+        "background": "yellow",
+        "hair": "orange",
+        "cover": "love",
+        "tokenId": 34
+    },
+    {
+        "background": "blue",
+        "hair": "yellow",
+        "cover": "casual",
+        "tokenId": 35
+    },
+    {
+        "background": "red",
+        "hair": "purple",
+        "cover": "love",
+        "tokenId": 36
+    },
+    {
+        "background": "red",
+        "hair": "orange",
+        "cover": "casual",
+        "tokenId": 37
+    },
+    {
+        "background": "orange",
+        "hair": "purple",
+        "cover": "casual",
+        "tokenId": 38
+    },
+    {
+        "background": "blue",
+        "hair": "blue",
+        "cover": "rock",
+        "tokenId": 39
+    },
+    {
+        "background": "yellow",
+        "hair": "orange",
+        "cover": "cool",
+        "tokenId": 40
+    },
+    {
+        "background": "orange",
+        "hair": "red",
+        "cover": "casual",
+        "tokenId": 41
+    },
+    {
+        "background": "yellow",
+        "hair": "orange",
+        "cover": "casual",
+        "tokenId": 42
+    },
+    {
+        "background": "red",
+        "hair": "orange",
+        "cover": "fancy",
+        "tokenId": 43
+    },
+    {
+        "background": "blue",
+        "hair": "purple",
+        "cover": "casual",
+        "tokenId": 44
+    },
+    {
+        "background": "purple",
+        "hair": "orange",
+        "cover": "casual",
+        "tokenId": 45
+    },
+    {
+        "background": "purple",
+        "hair": "red",
+        "cover": "cool",
+        "tokenId": 46
+    },
+    {
+        "background": "orange",
+        "hair": "red",
+        "cover": "cool",
+        "tokenId": 47
+    },
+    {
+        "background": "orange",
+        "hair": "purple",
+        "cover": "love",
+        "tokenId": 48
+    },
+    {
+        "background": "orange",
+        "hair": "yellow",
+        "cover": "casual",
+        "tokenId": 49
+    },
+    {
+        "background": "orange",
+        "hair": "purple",
+        "cover": "fancy",
+        "tokenId": 50
+    },
+    {
+        "background": "red",
+        "hair": "purple",
+        "cover": "fancy",
+        "tokenId": 51
+    },
+    {
+        "background": "yellow",
+        "hair": "blue",
+        "cover": "cool",
+        "tokenId": 52
+    },
+    {
+        "background": "orange",
+        "hair": "blue",
+        "cover": "love",
+        "tokenId": 53
+    },
+    {
+        "background": "red",
+        "hair": "orange",
+        "cover": "cool",
+        "tokenId": 54
+    },
+    {
+        "background": "yellow",
+        "hair": "blue",
+        "cover": "love",
+        "tokenId": 55
+    },
+    {
+        "background": "purple",
+        "hair": "purple",
+        "cover": "rock",
+        "tokenId": 56
+    },
+    {
+        "background": "red",
+        "hair": "blue",
+        "cover": "cool",
+        "tokenId": 57
+    },
+    {
+        "background": "purple",
+        "hair": "purple",
+        "cover": "fancy",
+        "tokenId": 58
+    },
+    {
+        "background": "purple",
+        "hair": "yellow",
+        "cover": "fancy",
+        "tokenId": 59
+    },
+    {
+        "background": "purple",
+        "hair": "orange",
+        "cover": "rock",
+        "tokenId": 60
+    },
+    {
+        "background": "yellow",
+        "hair": "orange",
+        "cover": "fancy",
+        "tokenId": 61
+    },
+    {
+        "background": "yellow",
+        "hair": "purple",
+        "cover": "casual",
+        "tokenId": 62
+    },
+    {
+        "background": "blue",
+        "hair": "blue",
+        "cover": "fancy",
+        "tokenId": 63
+    },
+    {
+        "background": "yellow",
+        "hair": "yellow",
+        "cover": "casual",
+        "tokenId": 64
+    },
+    {
+        "background": "orange",
+        "hair": "purple",
+        "cover": "rock",
+        "tokenId": 65
+    },
+    {
+        "background": "blue",
+        "hair": "orange",
+        "cover": "fancy",
+        "tokenId": 66
+    },
+    {
+        "background": "red",
+        "hair": "purple",
+        "cover": "rock",
+        "tokenId": 67
+    },
+    {
+        "background": "red",
+        "hair": "blue",
+        "cover": "love",
+        "tokenId": 68
+    },
+    {
+        "background": "red",
+        "hair": "purple",
+        "cover": "casual",
+        "tokenId": 69
+    },
+    {
+        "background": "yellow",
+        "hair": "purple",
+        "cover": "fancy",
+        "tokenId": 70
+    },
+    {
+        "background": "blue",
+        "hair": "purple",
+        "cover": "love",
+        "tokenId": 71
+    },
+    {
+        "background": "blue",
+        "hair": "yellow",
+        "cover": "love",
+        "tokenId": 72
+    },
+    {
+        "background": "purple",
+        "hair": "orange",
+        "cover": "love",
+        "tokenId": 73
+    },
+    {
+        "background": "red",
+        "hair": "purple",
+        "cover": "cool",
+        "tokenId": 74
+    },
+    {
+        "background": "purple",
+        "hair": "red",
+        "cover": "casual",
+        "tokenId": 75
+    },
+    {
+        "background": "yellow",
+        "hair": "blue",
+        "cover": "rock",
+        "tokenId": 76
+    },
+    {
+        "background": "orange",
+        "hair": "red",
+        "cover": "love",
+        "tokenId": 77
+    },
+    {
+        "background": "yellow",
+        "hair": "blue",
+        "cover": "casual",
+        "tokenId": 78
+    },
+    {
+        "background": "blue",
+        "hair": "blue",
+        "cover": "casual",
+        "tokenId": 79
+    },
+    {
+        "background": "purple",
+        "hair": "blue",
+        "cover": "casual",
+        "tokenId": 80
+    },
+    {
+        "background": "blue",
+        "hair": "red",
+        "cover": "love",
+        "tokenId": 81
+    },
+    {
+        "background": "purple",
+        "hair": "blue",
+        "cover": "rock",
+        "tokenId": 82
+    },
+    {
+        "background": "red",
+        "hair": "yellow",
+        "cover": "rock",
+        "tokenId": 83
+    },
+    {
+        "background": "red",
+        "hair": "blue",
+        "cover": "casual",
+        "tokenId": 84
+    },
+    {
+        "background": "yellow",
+        "hair": "orange",
+        "cover": "rock",
+        "tokenId": 85
+    },
+    {
+        "background": "orange",
+        "hair": "red",
+        "cover": "fancy",
+        "tokenId": 86
+    },
+    {
+        "background": "purple",
+        "hair": "orange",
+        "cover": "fancy",
+        "tokenId": 87
+    },
+    {
+        "background": "yellow",
+        "hair": "yellow",
+        "cover": "fancy",
+        "tokenId": 88
+    },
+    {
+        "background": "purple",
+        "hair": "yellow",
+        "cover": "casual",
+        "tokenId": 89
+    },
+    {
+        "background": "yellow",
+        "hair": "red",
+        "cover": "casual",
+        "tokenId": 90
+    },
+    {
+        "background": "blue",
+        "hair": "red",
+        "cover": "rock",
+        "tokenId": 91
+    },
+    {
+        "background": "orange",
+        "hair": "yellow",
+        "cover": "love",
+        "tokenId": 92
+    },
+    {
+        "background": "red",
+        "hair": "orange",
+        "cover": "rock",
+        "tokenId": 93
+    },
+    {
+        "background": "purple",
+        "hair": "yellow",
+        "cover": "love",
+        "tokenId": 94
+    },
+    {
+        "background": "yellow",
+        "hair": "red",
+        "cover": "cool",
+        "tokenId": 95
+    },
+    {
+        "background": "yellow",
+        "hair": "blue",
+        "cover": "fancy",
+        "tokenId": 96
+    },
+    {
+        "background": "blue",
+        "hair": "red",
+        "cover": "fancy",
+        "tokenId": 97
+    },
+    {
+        "background": "red",
+        "hair": "blue",
+        "cover": "fancy",
+        "tokenId": 98
+    },
+    {
+        "background": "red",
+        "hair": "red",
+        "cover": "casual",
+        "tokenId": 99
+    }
+]
